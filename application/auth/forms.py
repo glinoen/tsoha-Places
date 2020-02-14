44cbd0d963de388d241b41a1fff47b3999ef9047
glinoen <juho.linden@gmail.com>
2020-02-14 20:35:23 +0200

    registaration validation and text when there is no topics

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
-    username = StringField("Username")
-    password = PasswordField("Password")
+    username = StringField("Username", [validators.InputRequired(message=None)])
+    password = PasswordField("Password", [validators.InputRequired(message=None)])
   
     class Meta:
         csrf = False
